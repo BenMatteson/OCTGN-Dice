c12201b87a6402304cb03284ebe82d4e4c805bd9
Ben <maiahee@gmail.com>
2021-04-06 10:53:01 -0700

    basic dice game for OCTGN
    currently contains only d6
    
    includes lib (for now) containing more general functions

---
+++ Dice/Dice/dice.py
@@ -0,0 +1,105 @@
+import sys
+
+def rollTable(group, x=0, y=0):
+    mute()
+    roll([c for c in table if c.set == 'Dice'])
+
+def roll(cards, x=0, y=0):
+    mute()
+    notify("{} Rolls:".format(me))
+    if any(card.controller != me for card in cards): 
+        callBackOnComplete(
+            players,                            # for simplicity, we ask everyone
+            [                                   # to give us all the cards
+                ['giveControlTo', [me, cards]]
+            ],
+            ['_rollActual', [cards]]            # when they're all done, roll
+        )
+    else:
+        _rollActual(cards)
+
+def _rollActual(cards, name=None):
+    mute()
+    if len(cards) <= 0:
+        return
+    x, y, w, h = [int(val)
+                  for val in getGlobalVariable('diceRegion').split(',')]
+    minDist = int(getGlobalVariable('diceBuffer'))
+    points = getDistributedPoints(x, y, w, h, len(cards), minDist)
+    # rots = rndArray(0, 360, cards.count)
+    rolls = {}
+    results = rand(count=len(cards))
+    for i, card in enumerate(cards):
+        card.moveToTable(points[i][0], points[i][1])
+        # card.orientation = rots[i]
+        result = (results[i] % len(card.alternates)) + 1
+        card.alternate = '' if result == 1 else str(result)
+
+        cardName = card.alternateProperty('', 'Name') if name is None else name
+        rolls[cardName] = rolls.get(cardName, []) + [str(result)]
+
+    for n, res in rolls.items():
+        notify("{}: ".format(n) + ', '.join(res))
+
+
+def isDice(cards, x=0, y=0):
+    return all(card.set == 'Dice' for card in cards)
+
+# area only used for start point, should probably be small
+
+
+def getDistributedPoints(x, y, width, height, count, minDist, k=30):
+    mute()
+    if count <= 0 or width <= 0 or height <= 0:
+        return
+
+    first = (rnd(x, width), rnd(y, height))
+    active = [first]
+    ret = {first}
+    count -= 1
+
+    # anulus pattern to find values around a point
+    anulusOuter = int(1.5*minDist)
+    offsetAnulus = []
+    for i in range(-anulusOuter, anulusOuter+1):
+        for j in range(-anulusOuter, anulusOuter+1):
+            if i**2 + j**2 < anulusOuter**2 and i**2 + j**2 > minDist**2:
+                offsetAnulus.append((i, j))
+    # disc pattern to check for too-close points
+    offsetDisc = []
+    for i in range(-minDist, minDist+1):
+        for j in range(-minDist, minDist+1):
+            if i**2 + j**2 < minDist**2:
+                offsetDisc.append((i, j))
+
+    while count > 0 and len(active) > 0:
+        current = active[rand(0, len(active))]
+        randomValues = rand(0, len(offsetAnulus), k)
+        for i in range(k):
+            try:
+                offset = offsetAnulus[randomValues[i]]
+                point = (current[0] + offset[0], current[1] + offset[1])
+                for off in offsetDisc:
+                    if (point[0] + off[0], point[1] + off[1]) in ret:
+                        raise InvalidPoint()
+                active.append(point)
+                ret.add(point)
+                count -= 1
+                # break
+            except InvalidPoint:
+                pass
+        else:
+            active.remove(current)
+
+    ret = list(ret)
+    # this should never happen, but to be safe, we make sure to pad values if needed
+    while count > 0:
+        whisper("dice placement error: roll values are unaffected")
+        ret += [(x, y)]
+        count -= 1
+
+    return ret
+
+
+class InvalidPoint(Exception):
+    pass
---
+++ Dice/octgn-lib/lib.py
@@ -0,0 +1,174 @@
+# Version 0.0.3
+import sys
+
+RANDOM_NAME_SIZE = 20  # size of random unique names
+
+
+def giveControlTo(player, cards):
+    # region docstring
+    """
+    give control of cards to player
+
+    Parameters
+    ----------
+    player : player
+        The player who should be given the cards.
+    cards : list
+        A list of cards, not all cards need to be controlled
+    """
+    # endregion
+    mute()
+    for card in cards:
+        if card.controller == me:
+            card.controller = player
+
+# region remoteCall extensions
+# region chained calls
+
+
+def buildCallChain(callList):
+    # region docstring
+    """
+    Takes a list of calls and builds a list for use with chainedCall that executes them 
+    in order.
+
+    Parameters
+    ----------
+    callList : list
+        A list of "calls" to be performed in sequence.
+        each "call" is a list of player, function name as a string and
+        a list of arguments. Essentially the arguments of a remoteCall, as a list
+        Example:
+        [ 
+            [player, 'function', [arg1, arg2, etc.] ],
+            [player2, 'otherFunction', [] ]
+        ]
+    """
+    # endregion
+    mute()
+    return _internalBuildCallChain(callList, me)
+
+
+def _internalBuildCallChain(callList, prev):
+    mute()
+    if len(callList) == 0 or len(callList[0]) < 3:
+        return []
+    current = callList[0]
+    if current[0] == prev:  # not a remote call
+        return [current[1:]] + _internalBuildCallChain(callList[1:], prev)
+    else:  # setup remote call to chain
+        prev = current[0]
+        return [
+            ['remoteCall',
+                [current[0], 'chainedCall', [
+                        [[current[1], current[2]]] +
+                    _internalBuildCallChain(callList[1:], prev)
+                ]
+                ]
+             ]
+        ]
+
+
+def chainedCall(callChain):
+    # region docstring
+    """
+    Executes a callChain constructed by BuildCallChain
+    Parameters
+    ----------
+    callChain : list
+        A call chain; must be constructed by BuildCallChain
+    """
+    # endregion
+    mute()
+    for func, args in callChain:
+        if func == 'remoteCall':
+            remoteCall(*args)
+            break
+        else:
+            globals()[func](*args)
+# endregion
+
+
+# region multiple callback
+callBacks = {}
+
+
+def callBackOnComplete(players, callList, callBack):
+    # region docstring
+    """
+    remoteCalls to all players in players list to execute all calls in call List.
+    When all players have completed all calls, executes callBack for local player.
+
+    Parameters
+    ----------
+    players : list
+        A list of players who will execute the callls in callList
+    callList : list
+        A list of "calls" to be performed before the callback.
+        each "call" is a list of a function name as a string and
+        a list of arguments. Essentially the arguments of a 
+        remoteCall minus the player, as a list
+        Example:
+        [ 
+            ['function', [arg1, arg2, etc.] ],
+            ['otherFunction', [] ]
+        ]
+    callBack : list
+        A list consisting of a function name as a string and arguments as a list,
+        to be called after all players complete callList.
+        formatting is identical to a single "call" from the callList
+    """
+    # endregion
+    mute()
+    if len(players) <= 0:
+        return
+    # define random name for function
+    name = randomName()
+    setGlobalVariable(name, len(players))
+
+    callBacks[name] = callBack
+
+    for player in players:
+        chain = buildCallChain(
+            [[player] + call for call in callList] +
+            [[me, '_finishedCall', [name]]]
+        )
+        chainedCall(chain)
+
+
+def _finishedCall(name):
+    mute()
+    remaining = int(getGlobalVariable(name)) - 1
+    if remaining > 0:
+        setGlobalVariable(name, remaining)
+    else:
+        func, args = callBacks.pop(name)
+        globals()[func](*args)
+# endregion
+
+# endregion
+
+
+def rand(val1=sys.maxsize, val2=None, count=0):
+    # note: sys.maxsize breaks rnd, but we exclude the max value later
+    mute()
+    if val2 is None:
+        min = 0
+        max = val1 - 1  # exclude max value like usual
+    else:
+        min = val1
+        max = val2 - 1  # exclude max value like usual
+    if count <= 0:
+        return rnd(min, max)
+    else:
+        return rndArray(min, max, count)
+
+
+def randomName():
+    # region docstring
+    """
+    Generates a random string made of characters which are valid for global variable names
+    """
+    # endregion
+    genName = rndArray(35, 90, RANDOM_NAME_SIZE)
+    return ''.join(map(chr, genName))
